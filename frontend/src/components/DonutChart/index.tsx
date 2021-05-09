54c2bd96db47f325a7cfdfdb9580a8b49814c068
vinifbarbosa <mvfbarbos@msn.com>
2021-05-09 16:27:02 -0400

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,7 +24,7 @@ useEffect(() => {
        
     });
 },[]);
-    
+    //usado para teste fictício
     /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
