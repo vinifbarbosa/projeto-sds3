fdacdda9107a79764b13787986dd530b40ae4d26
vinifbarbosa <mvfbarbos@msn.com>
2021-05-09 19:00:52 -0400

    Change base_url

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -13,17 +13,17 @@ const DonutChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-useEffect(() => {
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-        setChartData({ labels: myLabels, series: mySeries });
-       
-    });
-},[]);
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+    }, []);
     //usado para teste fictício
     /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,1 @@
-    // definindo a base url do netlify o link do heroku
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL =  process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
