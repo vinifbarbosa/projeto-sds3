ff33b761a6a4c518e5e0eab2b6ce68949f8365c8
vinifbarbosa <mvfbarbos@msn.com>
2021-05-09 17:17:01 -0400

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080";
+    // definindo a base url do netlify o link do heroku
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
