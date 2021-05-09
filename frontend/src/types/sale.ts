e7eed0527238eb03b5d5001571327618e4af93dc
vinifbarbosa <mvfbarbos@msn.com>
2021-05-09 17:12:10 -0400

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -9,15 +9,15 @@ export type Sale = {
     seller: Seller;
 }
 
-export type Salepage = {
+export type SalePage = {
     content?: Sale[];
     last: boolean;
     totalPages: number;
-    totalElements: number;    
+    totalElements: number;
     size?: number;
     number: number;
-    first: boolean;   
-    numberOfElements?: number;   
+    first: boolean;
+    numberOfElements?: number;
     empty?: boolean;
 }
 
