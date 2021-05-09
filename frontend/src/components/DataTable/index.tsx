3fb21bd518d61366c8498e3dbdb6286d8ec1cc6a
vinifbarbosa <mvfbarbos@msn.com>
2021-05-08 20:53:37 -0400

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -19,7 +19,7 @@ function DataTable() {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
-                    
+
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
@@ -90,7 +90,7 @@ function DataTable() {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
-                    
+
                 </tbody>
             </table>
         </div>
